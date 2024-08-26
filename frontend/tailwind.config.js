03640d5177f35302d453d64b383472acb5be92b8
Franco <sentimientoutu@gmail.com>
2024-08-26 18:49:02 -0300

    theme implemented

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,35 @@ module.exports = {
     "./src/**/*.{html,js}",
     "../backend/app/**/*.{html,js}"
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#FAA916",
+          "secondary": "#6D676E",
+          "accent": "#96031A",
+          "neutral": "#18181E",
+          "base-100": "#FBFFFE",
+          "info": "#38bdf8",
+          "success": "#00d573",
+          "warning": "#d64c00",       
+          "error": "#da003d",
+          },
+        },
+      ],
+    },
   theme: {
+    colors: {
+      "primary": "#FAA916",
+      "secondary": "#6D676E",
+      "accent": "#96031A",
+      "neutral": "#18181E",
+      "base-100": "#FBFFFE",
+      "info": "#38bdf8",
+      "success": "#00d573",
+      "warning": "#d64c00",
+      "error": "#da003d",
+    },
     extend: {},
   },
   plugins: [
